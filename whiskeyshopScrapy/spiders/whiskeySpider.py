b7be8cd4e84924e51eab71c5067e69f4ac99c594
yao chengwei <tracyyaocw@gmail.com>
2022-06-10 19:09:08 +0800

    problem with item 9

--- whiskeyshopScrapy/spiders/whiskeySpider.py
+++ whiskeyshopScrapy/spiders/whiskeySpider.py
@@ -7,13 +7,28 @@ class WhiskeySpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for product in response.css("div.product.details.product-item-details"):
-            yield {
-                "product_name" : product.css("a.product-item-link::text").get(),
-                "product_link" : product.css("a").attrib["href"],
-                "prodcut_price" : product.css("span.price::text").get()
-            }
-        
+        product_info = response.css("div.product-item-info")
+        for product in product_info:
+            try:
+                yield {
+                    "product_name" : product.css("a.product-item-link::text").get(),
+                    "product_link" : product.css("a").attrib["href"],
+                    "prodcut_price" : product.css("span.price::text").get(),
+                    "product_img_url" : product.css("img").attrib["src"]
+                }
+            except:
+               yield {
+                    "product_name" : product.css("a.product-item-link::text").get(),
+                    "product_link" : product.css("a").attrib["href"],
+                    "prodcut_price" : product.css("span.price::text").get(),
+                    "product_img_url" : "unknown"
+                } 
+        # yield {
+        #     "product_name" : product_info[8].css("a.product-item-link::text").get(),
+        #     "product_link" : product_info[8].css("a").attrib["href"],
+        #     "prodcut_price" : product_info[8].css("span.price::text").get(),
+        #     "product_img_url" : product_info[8].css("img").attrib["src"]
+        # } 
         next_page = response.css("a.action.next").attrib["href"]
         if next_page is not None:
             yield response.follow(next_page, self.parse)
